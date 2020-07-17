681286a5305eb6bf4da39b950a325339a9297590
dimametal19 <dimametal19@gmail.com>
2020-07-17 17:02:02 +0300

    end 17 les

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Main() {
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path="/" exact>
+                    <HomeGuest/>
+                </Route>
+                <Route path="/about-us">
+                    <About/>
+                </Route>
+                <Route path="/terms">
+                    <Terms/>
+                </Route>
+            </Switch>
+            <Footer/>
+        </BrowserRouter>
+    )
+
+}
+
+ReactDOM.render(<Main/>, document.querySelector('#app'))
